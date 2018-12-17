b85223e9102af4dfae8882a28b93dc7480fc0de3
Trevia Griffin <saratgrffn@gmail.com>
2018-12-17 13:32:55 -0600

    stuck on assigning value to ID

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,39 @@
 
+    // set global variables
+var wins = 0;
+var losses = 0;
+var gempicked = 0;
+var totalscore = 0;
+
+$(document).ready(function() { 
+  
+
+    var random = Math.floor(Math.random() * 99) + 1;
+    $("#random").html(random);
+        });
+//generate random number for gems
+    
+    var bluerandom = Math.floor(Math.random() * 14)  + 1;
+    var greenrandom = Math.floor(Math.random() * 14)  + 1;
+    var redrandom = Math.floor(Math.random() * 14)  + 1;
+    var purplerandom = Math.floor(Math.random() * 14)  + 1;
+   
+    $("#blue").click(function(){
+        
+       $(totalscore).val(bluerandom);
+      });
+        
+  /*  $(".gem").on("click", function() {
+
+        var gempicked = $(this).val();
+        console.log("They picked" + gempicked);
+    }); */
+    
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $("#totalscore").html(totalscore); 
+ //function to reset game 
+
 
 
 
